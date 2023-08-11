f926df3ea1412ac99f2b67d17189a02000c0ef9f
sam <3161728@gmail.com>
2023-08-11 20:07:09 +0900

    Hero

--- lib/widgets/webtoon_card.dart
+++ lib/widgets/webtoon_card.dart
@@ -17,14 +17,6 @@ class WebtoonCard extends StatelessWidget {
       onTap: () {
         Navigator.push(
           context,
-          // MaterialPageRoute(
-          //   builder: (context) => DetailScreen(
-          //     title: title,
-          //     thumb: thumb,
-          //     id: id,
-          //   ),
-          //   fullscreenDialog: true,
-          // ),
           PageRouteBuilder(
             transitionsBuilder:
                 (context, animation, secondaryAnimation, child) {
@@ -38,26 +30,30 @@ class WebtoonCard extends StatelessWidget {
                 child: child,
               );
             },
-            pageBuilder: (context, anmation, secondaryAnimation) =>
+            pageBuilder: (context, animation, secondaryAnimation) =>
                 DetailScreen(id: id, title: title, thumb: thumb),
+            fullscreenDialog: true,
           ),
         );
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.3),
-                  )
-                ]),
-            child: Image.network(thumb),
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ]),
+              child: Image.network(thumb),
+            ),
           ),
           const SizedBox(
             height: 10,
